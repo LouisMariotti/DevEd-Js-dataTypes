7e7c19f05860f01bc30a490d402d30f66d639816
LouisMariotti <68160210+LouisMariotti@users.noreply.github.com>
2020-09-02 11:35:39 +0200

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,27 @@
+// Primitive Data Types
+
+/* A data type define what kind of data you can add on a variable */
+
+// Numbers
+const day = 5; // 5 is a number, this is a datatype
+// If we don't know what type of datatype we're using we can do
+console.log(typeof day); // Here it will say that it is a number
+
+// Strings
+const name = "Louis"; // String correspond to text, important to add "" to it. 
+console.log(typeof name); // it will say string
+
+// Booleans
+const test = true; // A boolean is a true or false value
+
+// Undefined
+let month; // This is an undefined variable, doesn't have a value yet assign to it
+console.log(month); // It's gonna say 'undefined'
+
+// Null
+let minute = null; // We explicitly assign a value of nothing to the variable
+
+// Symbol
+const name = Symbol(); 
+// Kinf of new, use for library, use as a unique identifier inside object
+
